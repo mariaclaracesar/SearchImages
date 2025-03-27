ea77d393bb89d574d6f3e6907b24a22ade479fdf
Maria Clara ( Ma ) <masaldanha@brq.com>
2025-03-26 23:26:59 -0300

    config babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@theme': './src/theme',
+          '@screens': './src/screens',
+          '@routes': './src/routes',
+        },
+      },
+    ],
+  ],
 };
