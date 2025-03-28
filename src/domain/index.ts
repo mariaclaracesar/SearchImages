397a67461adb6963c0cf7906c87cb07ece7a01f9
Maria Clara ( Ma ) <masaldanha@brq.com>
2025-03-28 00:56:32 -0300

    homeScreen

---
+++ src/domain/index.ts
@@ -0,0 +1,4 @@
+// export * from './Post/postAdapter'
+export * from './Post/postApi';
+export * from './Post/postService';
+export * from './Post/postTypes';
--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,21 +1,55 @@
-import {Box, Button, Text, TextInput} from '@components';
+import {Box, Screen, TextInput} from '@components';
+import {postService, UnsplashImage} from '@domain';
+import {useEffect, useState} from 'react';
 
 export function HomeScreen() {
-  return (
-    <Box padding="s24" flex={1} justifyContent="center">
-      <Text mb="s8" preset="headingLarge">
-        Olá!
-      </Text>
-      <Text preset="paragraphLarge">Procurando imagens pra se inspirar?</Text>
+  const [postList, setPostList] = useState<UnsplashImage[]>([]);
+  const [search, setSearch] = useState('');
 
-      <Box mt="s32">
+  const handleSearch = async () => {
+    const results = await postService.getList(search);
+    setPostList(results);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  });
+
+  return (
+    // <Box padding="s24" flex={1} justifyContent="center">
+    //   <Text mb="s8" preset="headingLarge">
+    //     Olá!
+    //   </Text>
+    //   <Text preset="paragraphLarge">Procurando imagens pra se inspirar?</Text>
+
+    //   <Box mt="s32">
+    //     <TextInput
+    //       placeholder="Ex: cachorros fofinhos"
+    //       label="Busque uma imagem"
+    //       value={search}
+    //       onChangeText={setSearch}
+    //     />
+
+    //     <Button marginTop="s24" title="Buscar" onPress={handleSearch} />
+
+    //     {/* {postList.map(post => (
+    //       <Box key={post.id} mt="s16">
+    //         <Text>{post.description || 'Sem descrição'}</Text>
+    //       </Box>
+    //     ))} */}
+
+    //   </Box>
+    // </Box>
+
+    <Screen>
+      <Box>
         <TextInput
           placeholder="Ex: cachorros fofinhos"
           label="Busque uma imagem"
+          value={search}
+          onChangeText={setSearch}
         />
-
-        <Button marginTop="s24" title="Buscar" />
       </Box>
-    </Box>
+    </Screen>
   );
 }
