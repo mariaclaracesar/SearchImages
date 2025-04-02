085e0c30a59e7728db1e4eb8b6b51fc1906e6ed4
Maria Clara ( Ma ) <masaldanha@brq.com>
2025-04-02 10:00:31 -0300

    safeArea provider

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 import {HomeScreen} from '@screens';
 import {ThemeProvider} from '@shopify/restyle';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {theme} from './theme';
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <SafeAreaProvider>
       <ThemeProvider theme={theme}>
         <HomeScreen />
       </ThemeProvider>
-    </>
+    </SafeAreaProvider>
   );
 }
 
