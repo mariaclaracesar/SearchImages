8f2b7cabf7636eabc5a7e731edd2d7325b76ca0a
Maria Clara ( Ma ) <masaldanha@brq.com>
2025-04-03 01:19:43 -0300

    create component icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './ActivityIndicator/ActivityIndicator';
 export * from './Box/Box';
 export * from './Button/Button';
+export * from './Icon/Icon';
 export * from './PostItem/PostItem';
 export * from './Screen/Screen';
 export * from './Text/Text';
